4ea67209ddaf5e8698ec037772e77b2b2462ab8d
ArthurSantDev <devarthursant@gmail.com>
2024-07-01 17:26:12 -0300

    Nav font semibold

--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -9,7 +9,7 @@ const Theme = () => {
 
   return (
     <div>
-    <div className='fixed bottom-10 right-6'>
+    <div className='fixed bottom-6 right-6'>
       <button  
         onClick={toggleTheme}
         className='theme'>
