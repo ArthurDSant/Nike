b7bb0e21eeda91bd63f6b0658672a6b02244ae8d
ArthurSantDev <devarthursant@gmail.com>
2024-07-01 17:00:25 -0300

    function

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import React from 'react';
 
-export function Card ({item, onClick}) {
+export function Card({ item, onClick }) {
+  const handleClick = (item) => {
+    onClick(item);
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   return (
     <div
-      onClick={() => onClick(item)} 
-      className={`${item.className} card`}>
+      onClick={() => handleClick(item)}
+      className={`${item.className} card`}
+    >
       <div className='p-8'>
         <div className='cardTitle'>{item.title}</div>
         <div className='cardShop'>SHOP NOW +</div>
       </div>
       <div>
-        <img src={item.src} className='cardImg'/>
+        <img src={item.src} className='cardImg' />
       </div>
     </div>
-  )
+  );
 }
 
